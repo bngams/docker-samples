faff5efc1143bcd68e34b2c58fb5209e71b938bd
root <n.boris@devidia.net>
2024-03-20 12:49:00 +0000

    simple config compose for loki

--- stop.sh
+++ stop.sh
@@ -3,5 +3,5 @@ export COMPOSE_FILES="$(find . | grep "docker-compose.yml")"
 # WIP --build as args ...
 for COMPOSE_FILE in $COMPOSE_FILES
 do
-    docker-compose -f $COMPOSE_FILE rm -f
+    docker-compose -f $COMPOSE_FILE down
 done
